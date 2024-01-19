25271c6f5b39e5b99d3536bdb206a8efc7bf025c
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2024-01-20 00:23:47 +0900

    get user info

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,55 @@
-import Form from 'react-bootstrap/Form';
+'use client';
+
+import { useEffect, useState } from 'react';
+
+type user = {
+  id: string,
+  name: string,
+  email: string,
+  password: string,
+  auth: string
+}
 
 export default function Home() {
-  return (
+  const [message, setMessage] = useState('');
+  const [users, setUsers] = useState<user[]>([]);
+  // ユーザー名
+  const [name, setName] = useState('');
+  // 自己紹介
+  const [intro, setIntro] = useState('');
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:5000')
+      .then((res) => res.json())
+      .then((data:any) => {        
+        // ユーザー名を入れる
+        setName(data[0].name)
+        // 自己紹介
+        setIntro(data[0].email)
+      });
+  }, []);
+
+  const handleUpdate = () => {
+    alert('更新しました');
+  }
+
+  return (    
     <>
       <div className="container mt-4">
         <div className="row justify-content-center">
-          <div className="col-md-9">
+          <div className="col-md-9">            
             <h2>マイページ</h2>
 
             {/* 名前入力フォーム */}
             <div className="mb-4">
               <label htmlFor="nameFormInput" className="form-label">名前</label>
-              <input type="text" className="form-control" id="nameFormInput" placeholder="Input Your Name" />
+              <input type="text" className="form-control" id="nameFormInput" placeholder="Input Your Name" defaultValue={name} onChange={(e) => setName(e.target.value)} />
             </div>
 
             {/* 性別選択 */}
             <label htmlFor="nameFormInput" className="form-label">性別</label>
             <div className="form-check">
-              <input type="radio" className="form-check-input" name='flexRadioDefault' id='flexSexRadio1' />
+              <input type="radio" className="form-check-input" name='flexRadioDefault' id='flexSexRadio1' defaultChecked />
               <label htmlFor="flexSexRadio" className="form-check-label1">男性</label>
             </div>
 
@@ -35,11 +67,11 @@ export default function Home() {
             {/* 自己紹介入力フォーム */}
             <div className="mb-3">
               <label htmlFor="introFormInput" className="form-label">自己紹介</label>
-              <textarea className="form-control" id="introFormInput" rows={3}></textarea>
+              <textarea className="form-control" id="introFormInput" rows={3} defaultValue={intro} onChange={(e) => setName(e.target.value)}></textarea>
             </div>
 
             <div className="center-block">
-              <button type='submit' className="btn btn-primary">更新する</button>
+              <button type='submit' onClick={handleUpdate} className="btn btn-primary">更新する</button>
             </div>
           </div>
         </div>
